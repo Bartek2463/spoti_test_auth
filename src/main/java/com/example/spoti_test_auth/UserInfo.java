87d624bdebbdfc4fb8417399b7d499e1a8d3425a
Bartlomiej <szybiakbartlomiej@gmail.com>
2022-10-26 10:46:18 +0200

    Test Spotify

--- src/main/java/com/example/spoti_test_auth/UserInfo.java
+++ src/main/java/com/example/spoti_test_auth/UserInfo.java
@@ -1,5 +1,10 @@
 package com.example.spoti_test_auth;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserInfo {
-    
+
+
+
 }
---
+++ src/main/java/com/example/spoti_test_auth/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.example.spoti_test_auth;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableOAuth2Sso
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.authorizeHttpRequests().antMatchers("/hello").authenticated();
+    }
+}
